40c57a6f44508b446b285bc1445a1113ef0648d2
Simonas kuprys <kuprys.simonas@gmail.com>
2023-06-26 11:12:37 +0300

    second tesst writen

--- test_interactive_learning_tool.py
+++ test_interactive_learning_tool.py
@@ -1,4 +1,10 @@
-from interactive_learning_tool import percentage_counter
+from interactive_learning_tool import percentage_counter,Reader
+
+def test_button():
+    button = Button("2")
+    button.number("3")
+    assert button.number == "3"
+    
 
 def test_percentage_counter():
     assert percentage_counter(10, 0) == 100
@@ -8,3 +14,12 @@ def test_percentage_counter():
     assert percentage_counter(1, 0) == 100
     assert percentage_counter(0, 1) == 0
     assert percentage_counter(1000000, 500000) == 67
+    
+def test_read_file():
+    try:
+        all_info = Reader.read_file()
+        assert isinstance(all_info, list)
+        assert len(all_info) > 0
+        assert all(isinstance(row, dict) for row in all_info)
+    except FileNotFoundError:
+        print("File does not exist.")
